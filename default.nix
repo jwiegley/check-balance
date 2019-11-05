52910f14c9f934796ad74bfee8a2bb52d4ea2849
John Wiegley <johnw@newartisans.com>
2019-11-05 15:51:58 -0800

    Updates

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
 { compiler    ? "ghc863"
+, system      ? builtins.currentSystem
 , doBenchmark ? false
 , doTracing   ? false
 , doStrict    ? false
@@ -7,6 +8,7 @@
 , pkgs        ? import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256; }) {
+    inherit system;
     config.allowUnfree = true;
     config.allowBroken = false;
   }
@@ -29,12 +31,27 @@ in haskellPackages.developPackage {
   };
 
   modifier = drv: pkgs.haskell.lib.overrideCabal drv (attrs: {
+    doCheck = false;
     executableSystemDepends = (attrs.executableSystemDepends or []) ++ [
       pact
       pkgs.python27
       pkgs.jq
       pkgs.bash
+      pkgs.curl
     ];
+
+    preBuild = ''
+      sed -i -e "s%\"balance\",%\"$out/bin/balance\",%" src/App.hs
+      sed -i -e 's%PACT=pact%PACT=${pact}/bin/pact%' balance
+      sed -i -e 's%\bcurl\b%${pkgs.curl}/bin/curl%' balance
+      sed -i -e 's%\bpython\b%${pkgs.python27}/bin/python%' balance
+      sed -i -e 's%\bjq\b%${pkgs.jq}/bin/jq%' balance
+    '';
+
+    postInstall = ''
+      mkdir -p $out/bin
+      cp balance $out/bin
+    '';
 
     inherit doBenchmark;
   });
